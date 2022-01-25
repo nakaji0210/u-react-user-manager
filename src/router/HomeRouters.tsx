6fcf412a87aa0155111bf466f0dc9de5202b585c
nakaji0210 <g843522r@gmail.com>
2022-01-26 08:50:45 +0900

    仮の404ページの設定を追加

--- src/router/HomeRouters.tsx
+++ src/router/HomeRouters.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../components/pages/Home";
+import { Page404 } from "../components/pages/Page404";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
 
@@ -10,6 +11,7 @@ return (
           <Route path="/" element={ <Home />} />
           <Route path="/user_management" element={ <UserManagement />} />
           <Route path="/setting" element={<Setting />} />
+          <Route path="/*" element={<Page404 />} />
 </Routes>
   )
 }
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { memo, VFC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
+import { Page404 } from "../components/pages/Page404";
 import { HomeRouters } from "./HomeRouters";
 
 export const Router: VFC = memo(() => {
@@ -10,6 +11,7 @@ export const Router: VFC = memo(() => {
       <Routes>
         <Route path="/" element={ <Login /> } />
         <Route path="home/*" element={<HomeRouters />} />
+        <Route path="/*" element={ <Page404 /> } />
       </Routes>
     </BrowserRouter>
   )

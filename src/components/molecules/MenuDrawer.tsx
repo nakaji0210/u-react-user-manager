257e5ec665cf3db0ebeb6cfab6f51da7d74e5881
nakaji0210 <g843522r@gmail.com>
2022-01-27 21:34:46 +0900

    ヘッダーのコンポーネント分割

---
+++ src/components/molecules/MenuDrawer.tsx
@@ -0,0 +1,31 @@
+import { memo, VFC } from "react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+} from "@chakra-ui/react";
+
+type Props = {
+  onClose: () => void;
+  isOpen: boolean;
+};
+
+export const MenuDrawer: VFC<Props> = memo((props) => {
+  const { isOpen, onClose } = props;
+
+  return (
+    <Drawer placement="right" size="xs" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerBody p={0} bg="gray.100">
+            <Button w="100%">TOP</Button>
+            <Button w="100%">ユーザー一覧</Button>
+            <Button w="100%">設定</Button>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  );
+});

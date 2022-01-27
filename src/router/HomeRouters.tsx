ebb45d349affd3bb6b41e4e4fa6150d38f374f74
nakaji0210 <g843522r@gmail.com>
2022-01-27 14:06:16 +0900

    ヘッダーの仮作成

--- src/router/HomeRouters.tsx
+++ src/router/HomeRouters.tsx
@@ -4,14 +4,17 @@ import { Home } from "../components/pages/Home";
 import { Page404 } from "../components/pages/Page404";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const HomeRouters = () => {
-return (
-  <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/user_management" element={ <UserManagement />} />
-          <Route path="/setting" element={<Setting />} />
-          <Route path="/*" element={<Page404 />} />
-</Routes>
-  )
-}
+  return (
+    <HeaderLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user_management" element={<UserManagement />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
+    </HeaderLayout>
+  );
+};
